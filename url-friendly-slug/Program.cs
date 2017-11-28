1bd22f80b58c8f34275763be3f88e25741269c11
Johan Boström <commit@zrx.se>
2017-11-28 22:40:26 +0100

    Add project files.

---
+++ url-friendly-slug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace url_friendly_slug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
